180dc1cd36193b75b7bd6626fb0491a68ab533d9
Dave McLain <dmclain@gmail.com>
2014-10-06 20:14:40 -0500

    v0.3.0 successfully export builtins

--- sentry_export/plugin.py
+++ sentry_export/plugin.py
@@ -1,7 +1,13 @@
+import csv
+from cStringIO import StringIO
+
 from django.utils.translation import ugettext_lazy as _
 from sentry.plugins import Plugin
+from django.http import HttpResponse
+from sentry.plugins.base import Response
 
 from sentry_export import VERSION
+from sentry_export.forms import ExportGroupForm
 
 
 class ExportPlugin(Plugin):
@@ -10,7 +16,7 @@ class ExportPlugin(Plugin):
     version = VERSION
 
     slug = "export"
-    title = _("Export")
+    title = _("Export Event Data")
     conf_title = title
     conf_key = slug
 
@@ -19,4 +25,33 @@ class ExportPlugin(Plugin):
         return action_list
 
     def view(self, request, group, **kwargs):
-        return self.render("sentry_export/export_form.html", {})
+        if request.method == 'GET':
+            form = ExportGroupForm()
+        else:
+            form = ExportGroupForm(request.POST)
+            if form.is_valid():
+                return self.render_events(form, group)
+        context = {
+            'title': self.title,
+            'form': form,
+        }
+        return self.render("sentry_export/export_form.html", context)
+
+    def render_events(self, form, group):
+        return CSVResponse(form, group.event_set.all())
+
+    def get_form(self, request, group):
+        return ExportGroupForm()
+
+
+class CSVResponse(Response):
+    def __init__(self, form, events):
+        self.form = form
+        self.events = events
+
+    def respond(self, *args, **kwargs):
+        f = StringIO()
+        writer = csv.DictWriter(f, self.form.get_fields())
+        writer.writeheader()
+        writer.writerows(self.form.get_values(evt) for evt in self.events)
+        return HttpResponse(f.getvalue(), mimetype="application/csv")
